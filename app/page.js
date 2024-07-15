1ad640f2e7f3c3a06c44ccd1a08598141ed407d0
Bianca Dominique De Torres <biancadominique.detorres@edu.sait.ca>
2024-07-15 13:44:59 -0600

    update page.js week 5

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
         <li><Link className={linkStyles} href="app\week-2\page.js">Week 2</Link> </li>
         <li><Link className={linkStyles} href="app\week-3">Week 3</Link> </li>
         <li><Link className={linkStyles} href="app\week-4">Week 4</Link> </li>
-        <li><Link className={linkStyles} href="app\week-5">Week 5</Link> </li>
+        <li><Link className={linkStyles} href="app\week-5\arrays\page.js">Week 5</Link> </li>
         <li><Link className={linkStyles} href="app\week-6">Week 6</Link> </li>
         <li><Link className={linkStyles} href="./page2/">Week 7</Link> </li>
       </ul>
