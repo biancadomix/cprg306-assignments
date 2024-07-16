0c54b538fe8b692787482f814fd2ab2259959a5f
Bianca Dominique De Torres <biancadominique.detorres@edu.sait.ca>
2024-07-16 16:36:41 -0600

    changed the color palette

--- app/page.js
+++ app/page.js
@@ -1,21 +1,21 @@
 import Link from "next/link";
 
 export default function Home() {
-  const linkStyles = "underline text-cyan-600 hover:text-cyan-300";
+  const linkStyles = "underline text-[#5D7599] hover:text-[#ABB6C8]";
 
   return (
-    <main className="h-screen bg-[#2b2d42] text-white flex flex-col items-center justify-center p-10">
+    <main className="h-screen bg-[#DADADA] text-[#2B2D42] flex flex-col items-center justify-center p-10">
       <div className="text-center mb-10">
-        <h1 className="text-5xl font-bold">Hello, I'm a Front end Developer</h1>
-        <p className="mt-4 text-lg">
-          Fear of creating web application designs and bringing them to life using code & creating mobile designs.
+        <h1 className="text-5xl font-bold text-[#5D7599]">Hello, I'm Bianca Dominique De Torres</h1>
+        <p className="mt-4 text-lg text-[#ABB6C8]">
+         I Fear of creating web application designs and bringing them to life using code & creating mobile designs. and I like trains
         </p>
-        <button className="mt-6 px-8 py-3 bg-red-500 text-white rounded-full hover:bg-red-400">
-          Create Journey
+        <button className="mt-6 px-8 py-3 bg-[#F7F0C6] text-[#2B2D42] rounded-full hover:bg-[#C2C4B6]">
+          Web Dev
         </button>
       </div>
       <div className="text-center">
-        <h2 className="text-3xl font-semibold mb-6">Application Links</h2>
+        <h2 className="text-3xl font-semibold text-[#5D7599] mb-6">Application Links</h2>
         <ul className="space-y-4">
           <li>
             <Link className={linkStyles} href="./page2/">
