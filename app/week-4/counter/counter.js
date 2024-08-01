1da4417570e483ec9bcdf17cb421f614bb1b5afa
Bianca Dominique De Torres <biancadominique.detorres@edu.sait.ca>
2024-08-01 14:55:29 -0600

    updated week 4

--- app/week-4/counter/counter.js
+++ app/week-4/counter/counter.js
@@ -3,9 +3,10 @@
 export default function Counter({currentCount, incrementCountFunction}){
 
     return (
-        <div className="bg-slate-300 p-s">
-            <h1 className="text-2x1">Increment Counter</h1>
-            <p className="text-1g"> Counter: {currentCount}</p> 
+        <div className="bg-slate-300 p-5">
+            <h1 className="text-2xl">Increment Counter</h1>
+            <p className="text-lg">Counter: {currentCount}</p>
+            <button onClick={incrementCountFunction} className="bg-blue-400 hover:bg-blue-700 active:bg-yellow-300 rounded text-white mt-5 px-4 py-2">Increment!</button>
         </div>
     )
 }
--- app/week-4/counter/page.js
+++ app/week-4/counter/page.js
@@ -1,16 +1,26 @@
-"use Client"
+"use client"
 
 import { useState } from "react";
-import counter from "./counter";
+import Counter from "./counter";
+export default function CounterPage() {
 
-export default function CounterPage () {
+    const [counter, setCounter] = useState(0);
+    const [timeCheck, setTimeCheck] = useState("");
 
-    const [ counter, setCounter ] = useState (0);
-    // const [timeCheck, setTimeCheck] = useState("");
-
-
+    
     const incrementCounter = () => {
         let currentCount = counter;
-        setCounter (currentCount + 1);
+        setCounter(currentCount + 1);
+        setTimeCheck("new time");
+        // counter = counter + 1; // don't do this
     }
+
+
+    return(
+        <main className="p-10">
+            {/* <p>{counter}</p>
+            <button onClick={incrementCounter}>Increment</button> */}
+            <Counter currentCount={counter} incrementCountFunction={incrementCounter} />
+        </main>
+    )
 }
